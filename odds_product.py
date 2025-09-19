fb7c3c25f84798f65b1864dbb4b35b48cfaad4fb
farhan03-s <farhanshaikshariff@gmail.com>
2025-09-19 15:53:14 +0530

    Create odds_product.py

---
+++ odds_product.py
@@ -0,0 +1,17 @@
+m = int(input("ENTER M VALUE: "))
+n = int(input("ENTER N VALUE: "))
+
+if m > n:
+    print(0)
+else:
+    x = 1
+    found = False  
+    for i in range(m, n + 1):
+        if i % 2 != 0:
+            x *= i
+            found = True
+
+    if found:
+        print("PRODUCT:", x)
+    else:
+        print(0)
